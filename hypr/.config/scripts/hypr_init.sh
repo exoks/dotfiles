694e94334eb00289483894599c136c12b4fe6d30
exoks <exoks.team@gmail.com>
2025-01-02 12:03:22 +0100

    hypr: change config structure

---
+++ hypr/.config/scripts/hypr_init.sh
@@ -0,0 +1,56 @@
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀            𓐓  hypr_init.sh 𓐔           
+#  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+#  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀   Student: oezzaou </var/spool/mail/oezzaou> 
+#  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+#  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/01/01 13:03:14 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/02 11:45:03 by oezzaou
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+#  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+#  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+#  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+#  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
+# INFO:====<[ autostart: script ]>=============================================
+#!/bin/bash
+
+# NOTE:> packages 
+hyprpaper="hyprpaper"
+hyprlock="hyprlock"
+waybar="waybar"
+wofi="wofi"
+
+# NOTE:> check if waybar exist otherwise install it
+if ! pacman -Qi $waybar &>/dev/null; then
+  echo "auto_start: $waybar
+  not found. Installing..."
+  pacman -S --noconfirm $waybar
+else
+  echo "auto_start: $waybar is already installed."
+fi
+
+# NOTE:> check if hyprpaper exist otherwise install it
+if ! pacman -Qi $hyprpaper &>/dev/null; then
+  echo "auto_start: $hyprpaper not found. Installing..."
+  pacman -S --noconfirm $hyprpaper
+else
+  echo "auto_start: $hyprpaper is already installed."
+fi
+
+# NOTE:> check if hyprlock exist otherwise install it
+if ! pacman -Qi $hyprlock &>/dev/null; then
+  echo "auto_start: $hyprlock not found. Installing..."
+  pacman -S --noconfirm $hyprlock
+else
+  echo "auto_start: $hyprlock is already installed."
+fi
+
+# NOTE:> check if wofi exist otherwise install it
+if ! pacman -Qi $wofi &>/dev/null; then
+  echo "auto_start: $wofi not found. Installing..."
+  pacman -S --noconfirm $wofi
+else
+  echo "auto_start: $wofi is already installed."
+fi
+
+# INFO:========================================================================
