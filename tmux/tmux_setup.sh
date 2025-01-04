35112787df5fb9e536fafea3e9c2181d8ad4b02e
exoks <exoks.team@gmail.com>
2025-01-05 00:23:23 +0100

    tmux: setup is done

---
+++ tmux/tmux_setup.sh
@@ -0,0 +1,52 @@
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀           𓐓  tmux_setup.sh 𓐔           
+#  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+#  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀   Student: oezzaou </var/spool/mail/oezzaou> 
+#  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+#  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/01/04 23:36:04 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/05 00:22:15 by oezzaou
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+#  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+#  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+#  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+#  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
+
+# ====<[ tmux_setup: script ]>=================================================
+#!/bin/bash
+
+TMUX_PKG="tmux"
+TMUX_CONF=~/.tmux.conf
+TMUX_TPM=~/.tmux/plugins/tpm
+
+# INFO:> Ensure tmux is installed
+if ! pacman -Qi $TMUX_PKG &>/dev/null; then
+  echo "tmux_setup: $TMUX_PKG not found. Installing..."
+  sudo pacman -S tmux
+else
+  echo "tmux_setup: $TMUX_PKG is already installed."
+fi
+
+# INFO:> Ensure tmux_plugin_manger is installed
+if [ -d $TMUX_TPM ]; then
+    echo "tpm is already installed at $TMUX_TPM."
+else
+    echo "tpm is not installed. Installing..."
+    # Clone the TMUX_TPM repository
+    git clone https://github.com/tmux-plugins/tpm $TMUX_TPM
+    if [ $? -eq 0 ]; then
+        echo "tpm successfully installed at $TMUX_TPM."
+    else
+        echo "Failed to install tpm at $TMUX_TPM."
+        exit 1
+    fi
+fi
+
+# INFO:> Stow .tmux.conf file & ignore tmux_setup.sh
+stow --ignore='.*\.sh$' -t ~ . 
+
+# INFO:> Install tmux plugins
+echo "checking for plugins ..."
+sh "$TMUX_TPM/bin/install_plugins"
+
+# =============================================================================
