3888ce7e8a7fd34cc881c2a568d53a33f66949f0
exoks <exoks.team@gmail.com>
2025-01-05 00:40:23 +0100

    neovim: setup script is done

--- nvim/nvim_setup.sh
+++ nvim/nvim_setup.sh
@@ -1,52 +1,31 @@
 #  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
-#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀           𓐓  tmux_setup.sh 𓐔           
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀           𓐓  nvim_setup.sh 𓐔           
 #  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
 #  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀   Student: oezzaou </var/spool/mail/oezzaou> 
 #  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
 #  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
-#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/01/04 23:36:04 by oezzaou
-#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/05 00:31:52 by oezzaou
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/01/05 00:29:27 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/05 00:33:09 by oezzaou
 #  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
 #  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
 #  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
 #  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
 #  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
 
-# ====<[ tmux_setup: script ]>=================================================
+# ====<[ nvim_setup: script ]>=================================================
 #!/bin/bash
 
-TMUX_PKG="tmux"
-TMUX_CONF=~/.tmux.conf
-TMUX_TPM=~/.tmux/plugins/tpm
+NVIM_PKG="neovim"
 
-# INFO:> Ensure tmux is installed
-if ! pacman -Qi $TMUX_PKG &>/dev/null; then
-  echo "tmux_setup: $TMUX_PKG not found. Installing..."
-  sudo pacman -S $TMUX_PKG
+# INFO:> Ensure nvim is installed
+if ! pacman -Qi $NVIM_PKG &>/dev/null; then
+  echo "nvim_setup: $NVIM_PKG not found. Installing..."
+  sudo pacman -S $NVIM_PKG
 else
-  echo "tmux_setup: $TMUX_PKG is already installed."
+  echo "nvim_setup: $NVIM_PKG is already installed."
 fi
 
-# INFO:> Ensure tmux_plugin_manger is installed
-if [ -d $TMUX_TPM ]; then
-    echo "tpm is already installed at $TMUX_TPM."
-else
-    echo "tpm is not installed. Installing..."
-    # Clone the TMUX_TPM repository
-    git clone https://github.com/tmux-plugins/tpm $TMUX_TPM
-    if [ $? -eq 0 ]; then
-        echo "tpm successfully installed at $TMUX_TPM."
-    else
-        echo "Failed to install tpm at $TMUX_TPM."
-        exit 1
-    fi
-fi
-
-# INFO:> Stow .tmux.conf file & ignore tmux_setup.sh
+# INFO:> Stow nvim config & ignore nvim_setup.sh  
 stow --ignore='.*\.sh$' -t ~ . 
-
-# INFO:> Install tmux plugins
-echo "checking for plugins ..."
-sh "$TMUX_TPM/bin/install_plugins"
 
 # =============================================================================
