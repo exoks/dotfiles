96e9cc3bb723f0f71fee76b15b852449e807d279
exoks <exoks.team@gmail.com>
2025-01-05 13:52:58 +0100

    zsh: setup script is done

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -5,21 +5,20 @@
 #  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
 #  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
 #  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/01/05 00:54:26 by oezzaou
-#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/05 13:23:04 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/05 13:49:02 by oezzaou
 #  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
 #  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
 #  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
 #  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
 #  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
 
-# ====<[ zsh_setup: script ]>=================================================
+# ====<[ zsh_setup: script ]>==================================================
 #!/bin/bash
 
 ZSH_PKG="zsh"
 OH_MY_ZSH_DIR=~/.oh-my-zsh
 P10K_DIR=$OH_MY_ZSH_DIR/custom/themes/powerlevel10k
-
-
+OH_MY_ZSH_URL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 
 # INFO:> Ensure zsh is installed
 if ! pacman -Qi $ZSH_PKG &>/dev/null; then
@@ -34,7 +33,7 @@ if [ -d $OH_MY_ZSH_DIR ]; then
     echo "Oh My Zsh is already installed at $OH_MY_ZSH_DIR."
 else
     echo "Oh My Zsh is not installed. Installing..."
-    RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    RUNZSH=no sh -c "$(curl -fsSL $OH_MY_ZSH_URL)"
     if [ $? -eq 0 ]; then
         echo "Oh My Zsh successfully installed."
     else
@@ -54,9 +53,8 @@ else
 fi
 
 # INFO:> Stow zsh config & ignore zsh_setup.sh  
-stow --override=".zshrc|.p10k.zsh" --ignore='.*\.sh$' -t ~ . # --adopt 
-
+if [ -f ~/.zshrc ]; then
+  rm -rf ~/.zshrc
+fi
+stow --ignore='.*\.sh$' --override=".zshrc|.p10k.zsh" --adopt -t ~ . 
 # ==============================================================================
-# install zsh (done)
-# install oh-my-zsh
-# install powerlevel10k 
