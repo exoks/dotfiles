77e659fdf4c1d8459d853b00dcf0775fe3c8798b
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2025-02-08 21:51:38 +0100

    scripts: add macos_switcher.sh script

---
+++ scripts/macos_switcher.sh
@@ -0,0 +1,42 @@
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀       𓐓  macos_switcher.sh 𓐔           
+#  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+#  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀  Student: oezzaou <oussama.ezzaou@gmail.com> 
+#  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+#  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/02/08 21:01:12 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/02/08 21:19:08 by oezzaou
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+#  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+#  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+#  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+#  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
+
+#====<[ script: wallpaper switcher ]>==========================================
+#!/bin/bash
+
+# A varibale that store the dir's wallpapers.
+wallpaper_dir="/Users/oezzaou/dotfiles/hypr/.config/backgrounds"
+
+# INFO: You need to install 'coreutils' to use it, run brew install  coeutils
+# wallpaper=$(find "$wallpaper_dir" \
+# -type f \( -iname "*.jpg" -o -iname "*.png" \) | gshuf -n 1)
+
+# This command choses a random wallpaper from $wallpaper_dir directory 
+wallpaper=$(find "$wallpaper_dir" \
+  -type f \( -iname "*.jpg" -o -iname "*.png" \) | sort -R | head -n 1)
+
+# NOTE:====================================================================
+# - I am using 'sort -R' to chose a random wallpaper, You could use       | 
+# 'gshuf' also to do that, I have chosen 'sort -R' instead, because it    | 
+# does not require any additional or external packages.                   |
+# =========================================================================
+
+# A simple command for debuging purposes. 
+echo "switcher: set up ${wallpaper} wallpaper"
+
+# This command sets up $wallpaper as a desktop background.  
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db \
+  "update data set value = '$wallpaper'" && killall Dock
+
+#==============================================================================
