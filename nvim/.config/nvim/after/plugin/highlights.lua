c7482a36af5d5692825de4d5265cc55709873354
exoks <oussama.ezzaou@gmail.com>
2026-05-04 19:10:51 +0100

    nvim: fix: tree-sitter for neovim 12

--- nvim/.config/nvim/after/plugin/highlights.lua
+++ nvim/.config/nvim/after/plugin/highlights.lua
@@ -5,7 +5,7 @@
 --  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀
 --  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
 --  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2026/01/20 23:24:53 by oezzaou
---  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2026/02/03 11:44:54 by oezzaou
+--  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2026/05/04 16:01:13 by oezzaou
 --  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀
 --  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀
 --  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪
@@ -17,4 +17,4 @@ vim.opt.cursorline = true -- enable the `cursor column` [CursorColumn: default]
 vim.opt.colorcolumn = "80" -- ColorColumn at position `80` [ColorColumn ]
 
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2a2a2a" })
--- vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2a2a2a" })
+-- vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
--- nvim/.config/nvim/lua/oezzaou/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/oezzaou/plugins/treesitter.lua
@@ -5,7 +5,7 @@
 --  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀
 --  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
 --  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2026/01/19 19:45:40 by oezzaou
---  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2026/02/06 15:35:07 by oezzaou
+--  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2026/05/04 15:57:05 by oezzaou
 --  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀
 --  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀
 --  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪
@@ -14,23 +14,54 @@
 
 -- ===[ treesitter: ]===========================================================
 return {
-	-- plugin to generate AST used for highlights --
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "lua", "python", "javascript" },
-		auto_install = true, -- Automatically install missing parsers
-		highlight = {
-			enable = true, -- Enables treesitter-based syntax highlighting
-		},
-		indent = {
-			enable = true,
-		},
-	},
-	-- treesitter config --
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+  -- plugin to generate AST used for highlights --
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+
+  -- treesitter config --
+  config = function()
+    local ts = require("nvim-treesitter")
+
+    ts.install({
+      "lua",
+      "vim",
+      "python",
+      "bash",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "json",
+      "toml",
+      "yaml",
+      "c",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "lua",
+        "vim",
+        "python",
+        "bash",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "toml",
+        "yaml",
+        "c",
+        "cpp"
+      },
+
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
+
+    })
+
+  end,
 }
 
 -- NOTE:[ Ensure that `curl` and `tar` commands are INSTALLED ]
